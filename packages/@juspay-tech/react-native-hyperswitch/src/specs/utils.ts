ee9b375f625c99adf7991a73010758d50d884950
sh-iv-am <shivam.shashank@juspay.in>
2026-03-06 13:57:15 +0530

    chore: added utils.ts

---
+++ packages/@juspay-tech/react-native-hyperswitch/src/specs/utils.ts
@@ -0,0 +1,15 @@
+/**
+ * Allows using types that codegen doesn't support, which will be generated
+ * as mixed, but keeping the TS type for type-checking.
+ *
+ * Note that for some reason this only works for native components, not for turbo modules.
+ */
+export type UnsafeMixed<T> = T;
+
+/**
+ * Allows using types that codegen doesn't support, which will be generated
+ * as object, but keeping the TS type for type-checking.
+ *
+ * Note that for some reason this only works for turbo modules, not for native components.
+ */
+export type UnsafeObject<T> = T;
