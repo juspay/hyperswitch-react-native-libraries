3f7e6c2ecea27360121c61a6bb649582fd2b169d
Shivam <shivam.nan@juspay.in>
2026-04-09 12:42:48 +0530

    fix: example app

--- packages/@juspay-tech/react-native-hyperswitch/ios/hyperswitchSDK/Core/NativeModule/HyperModule.swift
+++ packages/@juspay-tech/react-native-hyperswitch/ios/hyperswitchSDK/Core/NativeModule/HyperModule.swift
@@ -28,7 +28,7 @@ internal class HyperModule: RCTEventEmitter {
 
     @objc
     internal override func supportedEvents() -> [String] {
-        return ["confirm", "confirmEC", "initHeadless", "triggerWidgetAction"]
+        return ["confirm", "confirmEC", "initHeadless", "triggerWidgetAction", "updateIntentInit", "updateIntentComplete"]
     }
 
     @objc
